9a780c46c5db1324496ff8112b94c8b31fea5378
H. Joe Lee <hyoklee@hdfgroup.org>
2025-06-06 13:00:14 -0500

    docs: add OMNI class

---
+++ omni/OMNI.h
@@ -0,0 +1,22 @@
+///
+/// OMNI.h
+/// 
+namespace CAE {
+  class OMNI {
+  public:
+    OMNI() = default;
+    ~OMNI() = default;
+    void Put() {
+      return 0;
+    }
+    
+    void List() {
+      return 0;
+    }    
+    
+    void Get() {
+      return 0;
+    }
+  }
+
+} // namespace CAE
