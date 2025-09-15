a14b9fd3f5e7d524adcb49bd16bae4587ed63c2b
H. Joe Lee <hyoklee@hdfgroup.org>
2025-09-15 15:49:17 -0500

    build: make ares pass

--- omni/aws.sh
+++ omni/aws.sh
@@ -3,7 +3,7 @@ mkdir aws
 cd aws
 cmake \
     -DCMAKE_TOOLCHAIN_FILE=/home/hyoklee/vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DAWS=ON -DPOCO=ON -DSITE="ubu-24.04/WSL" -DBUILDNAME="omni/r/poco+aws" \
+    -DUSE_AWS=ON -DUSE_POCO=ON -DSITE="ares" -DBUILDNAME="omni/r/poco+aws" \
     ..
 ctest -T Build
 ctest -C Release -T  Test
--- omni/poco.sh
+++ omni/poco.sh
@@ -1,7 +1,9 @@
 rm -rf poco
 mkdir poco
 cd poco
-cmake -DPOCO=ON -DSITE="ubu-24.04/WSL" -DBUILDNAME="omni/r" ..
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=/home/hyoklee/vcpkg/scripts/buildsystems/vcpkg.cmake \
+    -DUSE_POCO=ON -DSITE="ares" -DBUILDNAME="omni/r/poco" ..
 ctest -T Build
 ctest -C Release -T  Test
 cd ..
